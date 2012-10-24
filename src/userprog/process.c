ee8bb355bd947f032324b35fd145fa01d23151bd
Galen Knapp <galenknapp@gmail.com>
2012-10-24 16:28:45 -0700

    Worked on argument passing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -21,10 +21,22 @@
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
 
+#define MAX_ARGS 128
+
+struct arguments {
+  int num_args;
+  char *args[MAX_ARGS];
+}
+
 /* Starts a new thread running a user program loaded from
    FILENAME.  The new thread may be scheduled (and may even exit)
    before process_execute() returns.  Returns the new process's
    thread id, or TID_ERROR if the thread cannot be created. */
+
+// Our Idea: Tokenize the file_name parameter in process_execute
+// so that it now is an array of the arguments. Pass this to start_process
+// In load, this will be used to compute the stack offset, and then we will manually
+// set the addresses of the arguments under PHYS_BASE (right after the call to setup_stack)
 tid_t
 process_execute (const char *file_name) 
 {
@@ -38,8 +50,21 @@ process_execute (const char *file_name)
     return TID_ERROR;
   strlcpy (fn_copy, file_name, PGSIZE);
 
+  struct arguments arg_struct;
+
+  char *saveptr;
+  char *delim = " ";
+  char *cur = strtok_r(fn_copy, delim, &saveptr);
+  arg_struct.num_args = 0;
+  while (cur != NULL) {
+    arg_struct.args[arg_struct.num_args] = cur;
+    arg_struct.num_args++;
+    cur = strtok_r(NULL, delim, &saveptr);
+  }
+
+
   /* Create a new thread to execute FILE_NAME. */
-  tid = thread_create (file_name, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (file_name, PRI_DEFAULT, start_process, &arg_struct);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
@@ -48,9 +73,9 @@ process_execute (const char *file_name)
 /* A thread function that loads a user process and starts it
    running. */
 static void
-start_process (void *file_name_)
+start_process (void *args_)
 {
-  char *file_name = file_name_;
+  struct arguments *args = args_;
   struct intr_frame if_;
   bool success;
 
@@ -59,10 +84,10 @@ start_process (void *file_name_)
   if_.gs = if_.fs = if_.es = if_.ds = if_.ss = SEL_UDSEG;
   if_.cs = SEL_UCSEG;
   if_.eflags = FLAG_IF | FLAG_MBS;
-  success = load (file_name, &if_.eip, &if_.esp);
+  success = load (args, &if_.eip, &if_.esp);
 
   /* If load failed, quit. */
-  palloc_free_page (file_name);
+  palloc_free_page (args.args);
   if (!success) 
     thread_exit ();
 
@@ -206,7 +231,7 @@ static bool load_segment (struct file *file, off_t ofs, uint8_t *upage,
    and its initial stack pointer into *ESP.
    Returns true if successful, false otherwise. */
 bool
-load (const char *file_name, void (**eip) (void), void **esp) 
+load (const struct arguments *args, void (**eip) (void), void **esp) 
 {
   struct thread *t = thread_current ();
   struct Elf32_Ehdr ehdr;
@@ -222,10 +247,10 @@ load (const char *file_name, void (**eip) (void), void **esp)
   process_activate ();
 
   /* Open executable file. */
-  file = filesys_open (file_name);
+  file = filesys_open (args.args);
   if (file == NULL) 
     {
-      printf ("load: %s: open failed\n", file_name);
+      printf ("load: %s: open failed\n", args.args);
       goto done; 
     }
 
@@ -238,7 +263,7 @@ load (const char *file_name, void (**eip) (void), void **esp)
       || ehdr.e_phentsize != sizeof (struct Elf32_Phdr)
       || ehdr.e_phnum > 1024) 
     {
-      printf ("load: %s: error loading executable\n", file_name);
+      printf ("load: %s: error loading executable\n", args.args);
       goto done; 
     }
 
@@ -304,6 +329,37 @@ load (const char *file_name, void (**eip) (void), void **esp)
   /* Set up stack. */
   if (!setup_stack (esp))
     goto done;
+
+  // Make the magic happen
+  void *stack_pointers[args.num_args];
+  int i;
+  char *top = (char *) PHYS_BASE;
+  for (i = args.num_args - 1; i >= 0; i--) {
+    int total_length = strlen(args[i]) + 1;
+    char *dest = top - total_length;
+    memcpy((void *) dest, (void *) args[i], total_length);
+    stack_pointers[i] = (void *) dest;
+    top = dest;
+  }
+
+  int extra_bytes = (4 - (top % 4)) % 4;
+  int zero = 0;
+  for (i = 0; i < extra_bytes; i++) {
+    memcpy(top - 1, &zero, 1);
+    top--;
+  }
+
+  memcpy(top - 4, &zero, 4);
+  for (i = args.num_args - 1; i >= 0; i--) {
+    memcpy(top - 4, &stack_pointers[i], 4);
+    top -= 4;
+  }
+
+  memcpy(top - 4, &(args.num_args), 4);
+  top -= 4;
+  memcpy(top - 4, &zero, 4);
+  top -= 4;
+  *esp = (void *) top;
 
   /* Start address. */
   *eip = (void (*) (void)) ehdr.e_entry;
